e28f2326d636ee3d90c5c6d9f8d9bc66bb70be76
Gpu <142077517+kyubiner@users.noreply.github.com>
2024-09-25 22:58:00 +0700

    test

--- service-worker.js
+++ service-worker.js
@@ -23,7 +23,6 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -33,10 +32,8 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+        return response || fetch(event.request);
       })
   );
 });
+
