eff010abae3ad96cdf4d14270fc437d786a4ccf2
Gpu <142077517+kyubiner@users.noreply.github.com>
2024-09-26 19:06:00 +0700

    bug fixing none

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', function() {
+//         navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+//             console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//         }, function(err) {
+//             console.log('ServiceWorker registration failed: ', err);
+//         });
+//     });
+// }
 
 const CACHE_NAME = 'my-site-cache-v1';
 const urlsToCache = [
@@ -15,8 +15,8 @@ const urlsToCache = [
   '/styles.css',
   '/script.js',
   '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/image/icon.png',
+  '/image/icon.png'
 ];
 
 self.addEventListener('install', function(event) {
